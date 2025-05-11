69adf9b1d902895c395fb6bc2b30ef6770013a37
Klimenko George <george_klimenko6@mail.ru>
2025-05-11 23:25:58 +0300

    Фиксы

--- config/config.go
+++ config/config.go
@@ -1,65 +1,55 @@
 package config
 
-import (
-	"time"
-)
-
-type ApiGatewayConfig struct {
-	Http struct {
-		Host string `yaml:"host"`
-		Port int    `yaml:"port"`
-	} `yaml:"http"`
-	Services struct {
-		Auth struct {
-			URL string `yaml:"url"`
-		} `yaml:"auth"`
-		Calc struct {
-			URL string `yaml:"url"`
-		} `yaml:"calc"`
-	} `yaml:"services"`
-	Router string `yaml:"router"`
-	Logger string `yaml:"logger"`
-}
-
-type AuthConfig struct {
-	Http struct {
-		Host         string        `yaml:"host"`
-		Port         int           `yaml:"port"`
-		ReadTimeout  time.Duration `yaml:"read_timeout"`
-		WriteTimeout time.Duration `yaml:"write_timeout"`
-	} `yaml:"http"`
-	Database struct {
-		DSN string `yaml:"dsn"`
-	} `yaml:"database"`
-	Jwt struct {
-		Secret             string        `yaml:"secret"`
-		AccessTokenExpiry  time.Duration `yaml:"access_token_expiry"`
-		RefreshTokenExpiry time.Duration `yaml:"refresh_token_expiry"`
-	} `yaml:"jwt"`
-	Logger string `yaml:"logger"`
-	Router string `yaml:"router"`
-}
-
-type CalcOrchestratorConfig struct {
-	Port                 int    `yaml:"port"`
-	TimeAdditionMs       uint64 `yaml:"time_addition_ms"`
-	TimeSubtractionMs    uint64 `yaml:"time_subtraction_ms"`
-	TimeMultiplicationMs uint64 `yaml:"time_multiplication_ms"`
-	TimeDivisionMs       uint64 `yaml:"time_division_ms"`
-}
-
-type CalcAgentConfig struct {
-	Workers int           `yaml:"workers"`
-	Timeout time.Duration `yaml:"timeout"`
-}
-
-type CalcConfig struct {
-	Orchestrator CalcOrchestratorConfig `yaml:"orchestrator"`
-	Agent        CalcAgentConfig        `yaml:"agent"`
-}
+import "time"
 
 type Config struct {
-	ApiGateway ApiGatewayConfig `yaml:"api_gateway"`
-	Auth       AuthConfig       `yaml:"auth"`
-	Calc       CalcConfig       `yaml:"calc"`
+	ApiGateway struct {
+		Http struct {
+			Host string `mapstructure:"host"`
+			Port int    `mapstructure:"port"`
+		} `mapstructure:"http"`
+		Services struct {
+			Auth struct {
+				URL string `mapstructure:"url"`
+			} `mapstructure:"auth"`
+			Calc struct {
+				URL string `mapstructure:"url"`
+			} `mapstructure:"calc"`
+		} `mapstructure:"services"`
+		Router string `mapstructure:"router"`
+		Logger string `mapstructure:"logger"`
+	} `mapstructure:"api_gateway"`
+
+	Auth struct {
+		Http struct {
+			Host         string        `mapstructure:"host"`
+			Port         int           `mapstructure:"port"`
+			ReadTimeout  time.Duration `mapstructure:"read_timeout"`
+			WriteTimeout time.Duration `mapstructure:"write_timeout"`
+		} `mapstructure:"http"`
+		Database struct {
+			DSN string `mapstructure:"dsn"`
+		} `mapstructure:"database"`
+		Jwt struct {
+			Secret             string        `mapstructure:"secret"`
+			AccessTokenExpiry  time.Duration `mapstructure:"access_token_expiry"`
+			RefreshTokenExpiry time.Duration `mapstructure:"refresh_token_expiry"`
+		} `mapstructure:"jwt"`
+		Logger string `mapstructure:"logger"`
+		Router string `mapstructure:"router"`
+	} `mapstructure:"auth"`
+
+	Calc struct {
+		Orchestrator struct {
+			Port                 int `mapstructure:"port"`
+			TimeAdditionMs       int `mapstructure:"time_addition_ms"`
+			TimeSubtractionMs    int `mapstructure:"time_subtraction_ms"`
+			TimeMultiplicationMs int `mapstructure:"time_multiplication_ms"`
+			TimeDivisionMs       int `mapstructure:"time_division_ms"`
+		} `mapstructure:"orchestrator"`
+		Agent struct {
+			Workers int           `mapstructure:"workers"`
+			Timeout time.Duration `mapstructure:"timeout"`
+		} `mapstructure:"agent"`
+	} `mapstructure:"calc"`
 }
